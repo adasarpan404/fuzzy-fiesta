b7d361c9185cc43798d13ebb38f01537ee1efe42
Arpan Das <dasarpan471@gmail.com>
2022-07-24 12:11:55 +0530

    Literal Completed

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # fuzzy-fiesta
 
 In this repository, I have put all my algorithm and DSA problem for revision.
+
+
+Also, Put the code of new language I am learning
+
+
---
+++ Rust/Literal.rs
@@ -0,0 +1,39 @@
+// Integers 1, floats 1.2, characters 'a', strings "abc", booleans true and the unit type () can be expressed using literals.
+
+// Integers can, alternatively, be expressed using hexadecimal, octal or binary notation using these prefixes respectively: 0x, 0o or 0b.
+
+// Underscores can be inserted in numeric literals to improve readability, e.g. 1_000 is the same as 1000, and 0.000_001 is the same as 0.000001.
+
+// We need to tell the compiler the type of the literals we use. For now, we'll use the u32 suffix to indicate that the literal is an unsigned 32-bit integer, and the i32 suffix to indicate that it's a signed 32-bit integer.
+
+fn main(){
+
+    //Integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    println!("1 + 2 = {}", 1i32 + 2);
+
+    //Integer Subtraction 
+
+    println!("1 - 2 = {}", 1i32 -2);
+    
+    // Donot use u32 while subtracting it will show overflow
+
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+
+    println!("true OR false is {}", true || false);
+
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
+}
